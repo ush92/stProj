1b190f62f1618b9348b61e6a59852c9497934b0a
ush <ush1992@gmail.com>
2022-01-08 00:40:00 +0100

    init

---
+++ stProj-front/stproj-ui/src/Variables.js
@@ -0,0 +1,4 @@
+export const variables={
+    API_URL:"http://localhost:15730/api/",
+    PHOTO_URL:"http://localhost:15730/Photos/"
+}
---
+++ stProj/stProj/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace stProj.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+
+        public string DepartmentName { get; set; }
+    }
+}
